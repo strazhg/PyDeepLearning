9a0470d7211285a54e49f6067640d883bc61c36a
sg <icallhacks19@gmail.com>
2023-04-20 14:58:08 -0700

    Update DNN.py

--- py_deep_learning/DNN.py
+++ py_deep_learning/DNN.py
@@ -63,7 +63,7 @@ class DNN():
         for j in range(epoch):
             l = []
             for i in range(len(self.layers)):
-                out = self.f_forward(np.asarray(self.layers[i]))
+                out = self.f_forward(np.asarray(self.layers[i]).T)
                 l.append(self.loss(out, Y[i]))
                 self.back_prop(np.asarray(l[i]), Y, alpha)
             print("epochs:", j + 1, "======== acc:", (1-(sum(l)/len(self.layers)))*100)
